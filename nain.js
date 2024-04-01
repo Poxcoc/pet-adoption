8540173cd1f78258ef377962b480d7ec5046cf55
cervezatrescuervos@gmail.com <cervezatrescuervos@gmail.com>
2024-04-01 14:03:39 -0300

    finish synamic temperature feature

--- nain.js
+++ nain.js
@@ -4,12 +4,14 @@
 // La latitud de Miami, Florida, es 25.761681 y la longitud es -80.191788
 // https://api.weather.gov/zones/forecast/FLZ173
 
-async function start(){
+async function start() {
+    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+    const weatherData = await weatherPromise.json()
+  
+    const ourTemperature = weatherData.properties.periods[0].temperature
+    document.querySelector("#temperature-output").textContent = ourTemperature
     
-    const watherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
-    const watherData = await watherPromise.json();
-    document.querySelector("temperature-output").textContent = ourTemperature;
-
-    
-}
-start()
+  
+  }
+  
+  start()
