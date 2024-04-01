01d8470a5cb2ea7a02575c68696ec966670bf5ca
cervezatrescuervos@gmail.com <cervezatrescuervos@gmail.com>
2024-04-01 20:10:01 -0300

    Turned pet card into Js template

--- nain.js
+++ nain.js
@@ -1,17 +1,28 @@
-
-// https://api.weather.gov/points/{latitude},{longitude}
-// https://api.weather.gov/points/25.761681,-80.191788
-// La latitud de Miami, Florida, es 25.761681 y la longitud es -80.191788
-// https://api.weather.gov/zones/forecast/FLZ173
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
 
 async function start() {
-    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-    const weatherData = await weatherPromise.json()
-  
-    const ourTemperature = weatherData.properties.periods[0].temperature
-    document.querySelector("#temperature-output").textContent = ourTemperature
-    
-  
-  }
-  
-  start()
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
+}
+
+start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    const clone = template.content.cloneNode(true)
+
+    clone.querySelector("h3").textContent = pet.name
+
+    wrapper.appendChild(clone)
+  })
+  document.querySelector(".list-of-pets").appendChild(wrapper)
+}
+
+petsArea()
